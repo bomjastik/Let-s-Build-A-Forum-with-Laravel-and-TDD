412dc37160ce02adb51e438c95a8a2465feb0113
Anton Korchahin <anton.korchahin@gmail.com>
2018-07-23 15:43:24 +0300

    Episode 5 - The Reply Form

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -33,5 +33,35 @@
 
             </div>
         </div>
+
+        <div class="row mt-5">
+            <div class="col-md-12">
+                @auth()
+                    <form method="post" action="{{ route('threads.replies.store', $thread->id) }}" dusk="reply-form">
+                        {{ csrf_field() }}
+
+                        <div class="form-group">
+                            <textarea class="form-control @if ($errors->has('body')) is-invalid @endif" name="body"
+                                      id="body" rows="3" placeholder="Have something to say?" dusk="reply-body">
+                                {{ old('body') }}
+                            </textarea>
+                            @if ($errors->has('body'))
+                                <div class="invalid-feedback">
+                                    {{ $errors->first('body') }}
+                                </div>
+                            @endif
+                        </div>
+
+                        <button class="btn btn-primary" type="submit" dusk="add-reply">Add reply</button>
+                    </form>
+                @endauth
+
+                @guest()
+                    <div class="alert alert-light text-center" role="alert" dusk="login-alert">
+                        Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.
+                    </div>
+                @endguest
+            </div>
+        </div>
     </div>
 @endsection
--- tests/Browser/ThreadTest.php
+++ tests/Browser/ThreadTest.php
@@ -4,6 +4,7 @@ namespace Tests\Browser;
 
 use App\Reply;
 use App\Thread;
+use App\User;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -52,6 +53,59 @@ class ThreadTest extends DuskTestCase
 
             $browser->visit($thread->url())
                 ->assertSee($reply->body);
+        });
+    }
+
+    /**
+     * @test
+     * @throws \Throwable
+     */
+    public function an_authenticated_user_can_see_a_reply_form()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->resize(1920, 1080);
+
+            $thread = factory(Thread::class)->create();
+
+            $browser->loginAs(factory(User::class)->create())
+                ->visit($thread->url())
+                ->assertVisible('@reply-form');
+        });
+    }
+
+    /**
+     * @test
+     * @throws \Throwable
+     */
+    public function unauthenticated_user_can_not_see_a_reply_form()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->resize(1920, 1080);
+
+            $thread = factory(Thread::class)->create();
+
+            $browser->visit($thread->url())
+                ->assertMissing('@reply-form')
+                ->assertVisible('@login-alert');
+        });
+    }
+
+    /**
+     * @test
+     * @throws \Throwable
+     */
+    public function an_authenticated_user_can_submit_a_new_reply()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->resize(1920, 1080);
+
+            $thread = factory(Thread::class)->create();
+
+            $browser->loginAs(factory(User::class)->create())
+                ->visit($thread->url())
+                ->type('@reply-body', 'Test reply')
+                ->press('@add-reply')
+                ->assertSee('Test reply');
         });
     }
 }
